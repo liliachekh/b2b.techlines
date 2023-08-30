4123b31df93b22dca15717225f30150285dbbbcd
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-08-31 01:43:24 +0300

    UPDATE: add structure for custom dropdown checkbox

--- b2b/src/components/Filter/index.jsx
+++ b2b/src/components/Filter/index.jsx
@@ -1,7 +1,14 @@
 import styles from "./filter.module.scss";
 import { FilterIcon } from "../icons";
+import { useState } from "react";
 
 function Filter() {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+      };
 
     return (
         <div className={styles.filter}>
@@ -13,7 +20,21 @@ function Filter() {
                     </div>
                     <div className={styles.filter__navContent}>
                         <div className={styles.filter__dropdown}>
-
+                            <button className={styles.filter__dropdownBtn} onClick={toggleDropdown}>
+                                <span className="open">{selectedValues.length > 0 ? `Selected ${selectedValues.length} items` : 'Select'}</span>
+                            </button>
+                            {isDropdownOpen && (
+                                <ul>
+                                {filters?.categoriesFilters?.map((category) => (
+                                    <li key={category._id} className={styles.filter__sidebarItem}>
+                                    <label htmlFor={category._id}>
+                                        <input type="checkbox" id={category._id} name={category.name} data-filter-type="categories" onChange={valueChange} checked={selectedFilters.categories.includes(category.name) ? true : false} />
+                                        {category.name}
+                                    </label>
+                                    </li>
+                                ))}
+                                </ul>
+                            )}
                         </div>
                     </div>
                 </div>
