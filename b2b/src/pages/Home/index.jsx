a1e3419a68e2fbc4dfd2fbd6f10adb1fdf5983c8
Liliia Chekh <liliacheh@gmail.com>
2023-09-07 15:58:36 +0200

    add backToTop

--- b2b/src/pages/Home/index.jsx
+++ b2b/src/pages/Home/index.jsx
@@ -3,18 +3,20 @@ import Header from "../../components/Header";
 import ProductList from "../../components/ProductList";
 import { useQueryString } from "../../hooks";
 import { useGetAllProductsQuery } from "../../store/api/products.api";
-
+import BackToTop from "../../components/BackToTop"
+import Loader from "../../components/Loader";
 export function Home() {
   const { perPage } = useQueryString();
 
   const { data: products = [], isLoading } = useGetAllProductsQuery(window.location.search ? window.location.search : `?perPage=${perPage}`);
 
-  if (isLoading) return <h1>Loading</h1>
+  if (isLoading) return <Loader/>
 
   return (
     <>
       <Header />
       <ProductList {...products} />
+      <BackToTop/>
     </>
   )
 }
