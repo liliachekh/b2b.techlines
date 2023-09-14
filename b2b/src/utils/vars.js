ddf54bdbf87a9bcdb534503f809aa86fb3db4500
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-09-15 01:04:46 +0300

    UPDATE: add version filter with submit button, with 1 state

--- b2b/src/utils/vars.js
+++ b2b/src/utils/vars.js
@@ -1,1 +1,7 @@
-export const baseUrl = 'http://localhost:4000/api/';
+// export const baseUrl = 'http://localhost:4000/api/';
+export const baseUrl = 'https://storage.techlines.es/api/';
+
+export const headers = () => ({
+    Authorization: `Bearer ${localStorage.getItem('token') || ''}`,
+    'Content-Type': 'application/json',
+  })
