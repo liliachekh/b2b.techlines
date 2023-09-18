77617d60f8f6538fd36ac0bb45e6bf6df9c169b5
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-09-18 13:46:35 +0300

    UPDATE: add component FilterType, add hook useDebounce, correct filter.api, correct Filter component

--- b2b/src/components/Filter/index.jsx
+++ b2b/src/components/Filter/index.jsx
@@ -1,85 +1,50 @@
 import styles from "./filter.module.scss";
-import { FilterIcon, ArrowDropdown } from "../icons";
-import { useState, useEffect, useRef } from "react";
-import { useQueryString } from '../../hooks';
+import { FilterIcon } from "../icons";
+import { useState, useEffect, useCallback } from "react";
+import { useDebounce, useQueryString } from '../../hooks';
 import { useGetFiltersQuery } from "../../store/api/filter.api";
+import { FilterType } from "../FilterType";
 
 function Filter() {
   const { data: filtersBD = [] } = useGetFiltersQuery();
+  const filters = [...new Set(filtersBD.map((item) => item.type))];
   const { params, setSearchParams } = useQueryString();
 
-
-  // console.log(filtersBD);
-  // console.log(params);
-
-  const [isDropdownOpen, setIsDropdownOpen] = useState(null);
   const [isFilterOpen, setIsFilterOpen] = useState(false);
-  const dropdownRef = useRef(null);
+  const [searchValue, setSearchValue] = useState(params.search || '');
+
+  const debouncedValue = useDebounce(searchValue, 1000);
 
   const toggleFilter = () => {
     setIsFilterOpen(!isFilterOpen);
   };
 
-  const toggleDropdown = (type) => {
-    setIsDropdownOpen(isDropdownOpen === type ? null : type);
-  };
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
+  }
 
-  const handleDocumentClick = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setIsDropdownOpen('');
+  const search = useCallback(() => {
+    if (debouncedValue?.length >= 3) {
+      setSearchParams({ ...params, search: debouncedValue, startPage: 1 });
+    } else {
+      if (Object.keys(params).filter((param) => !['perPage', 'startPage'].includes(param)).length > 0) {
+        const newParams = { ...params };
+        delete newParams.search;
+        setSearchParams({ ...newParams, startPage: 1 });
+      } else {
+        setSearchParams({});
+      }
     }
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue])
 
   useEffect(() => {
-    document.addEventListener('click', handleDocumentClick);
-    return () => {
-      document.removeEventListener('click', handleDocumentClick);
-    };
-  }, []);
-
-  function handleCheckbox(key, value) {
-    if (params[key]?.includes(value)) {
-      const oldParam = params[key].split(',').filter(a => a !== value).join(',');
-
-      if (oldParam?.length > 0) {
-        setSearchParams({ ...params, [key]: oldParam, startPage: 1 })
-      } else {
-        const newParams = { ...params };
-        delete newParams[key];
-        setSearchParams({ ...newParams, startPage: 1 })
-      }
-    } else {
-      const oldParam = params[key] ? params[key] + ',' : '';
-      setSearchParams({ ...params, [key]: oldParam + value, startPage: 1 })
-    }
-  }
-
-  // const debounce = (fn, delay) => {
-  //   let timeoutID;
-
-  //   return function (...args) {
-  //     if (timeoutID) {
-  //       clearTimeout(timeoutID);
-  //     }
-
-  //     timeoutID = setTimeout(() => {
-  //       fn(...args);
-  //     }, delay);
-  //   };
-  // };
-
-  const handleSearch = (e) => {
-    if (e.target.value) {
-      setSearchParams({ ...params, search: e.target.value, startPage: 1 });
-    } else {
-      const newParams = { ...params };
-      delete newParams.search;
-      setSearchParams({ ...newParams, startPage: 1 })
-    }
-  }
+    search();
+  }, [search])
 
   const clearAllFilters = () => {
     setSearchParams({});
+    setSearchValue('');
   };
 
   return (
@@ -93,82 +58,24 @@ function Filter() {
             <FilterIcon />
             <span className={styles.filter__title}>Filters</span>
           </div>
-          <div
-            className={`${styles.filter__navContent} ${isFilterOpen && styles.open}`}>
-            <div className={styles.filter__dropdownBlock} ref={dropdownRef}>
-              <div className={styles.filter__dropdown}>
-                <h4 className={styles.filter__dropdownTitle}>Categories</h4>
-                <button
-                  type="button"
-                  className={styles.filter__dropdownBtn}
-                  onClick={() => toggleDropdown('categories')}>
-                  <span
-                    className={styles.filter__dropdownBtnText}>
-                    {params?.categories && params.categories.split(',').length > 0 ? `Selected ${params.categories.split(',').length} items` : 'Select'}
-                  </span>
-                  <ArrowDropdown />
-                </button>
-                <ul className={`${styles.filter__dropdownList} ${isDropdownOpen === 'categories' && styles.open}`}>
-                  {filtersBD?.map((category) => {
-                    if (category.type === 'categories')
-                      return (
-                        <li key={category._id} className={styles.filter__dropdownItem}>
-                          <label htmlFor={category.name}>
-                            <input
-                              type="checkbox"
-                              id={category.name}
-                              name={category.type}
-                              value={category.name}
-                              onChange={() => handleCheckbox(category.type, category.name)}
-                              checked={params[category.type]?.includes(category.name) ? true : false} />
-                            {category.name}
-                          </label>
-                        </li>)
-                        return null;
-                  })}
-                </ul>
-              </div>
-              <div className={styles.filter__search}>
-                <h4 className={styles.filter__dropdownTitle}>Search product</h4>
-                <input
-                  type="text"
-                  id="search"
-                  name="search"
-                  onChange={handleSearch}
-                  value={params?.search || ''} />
-              </div>
-              <div className={styles.filter__dropdown}>
-                <h4 className={styles.filter__dropdownTitle}>Brand</h4>
-                <button
-                  type="button"
-                  className={styles.filter__dropdownBtn}
-                  onClick={() => toggleDropdown('brand')}>
-                  <span
-                    className={styles.filter__dropdownBtnText}>
-                    {params?.brand && params.brand.split(',').length > 0 ? `Selected ${params.brand.split(',').length} items` : 'Select'}
-                  </span>
-                  <ArrowDropdown />
-                </button>
-                <ul className={`${styles.filter__dropdownList} ${isDropdownOpen === 'brand' && styles.open}`}>
-                  {filtersBD?.map((category) => {
-                    if (category.type === 'brand')
-                      return (
-                        <li key={category._id} className={styles.filter__dropdownItem}>
-                          <label htmlFor={category.name}>
-                            <input
-                              type="checkbox"
-                              id={category.name}
-                              name={category.type}
-                              value={category.name}
-                              onChange={() => handleCheckbox(category.type, category.name)}
-                              checked={params[category.type]?.includes(category.name) ? true : false} />
-                            {category.name}
-                          </label>
-                        </li>)
-                        return null;
-                  })}
-                </ul>
-              </div>
+          <div className={`${styles.filter__navContent} ${isFilterOpen && styles.open}`}>
+            <div className={styles.filter__dropdownBlock}>
+              {filters?.map((type) => (
+                <FilterType
+                  key={type}
+                  type={type}
+                  items={filtersBD.filter((category) => category.type === type)} />
+              ))}
+            </div>
+            <div className={styles.filter__search}>
+              <h4 className={styles.filter__dropdownTitle}>Search product</h4>
+              <input
+                type="text"
+                id="search"
+                name="search"
+                onChange={handleSearch}
+                value={searchValue}
+              />
             </div>
             <div className={styles.filter__navigation}>
               <button
@@ -177,11 +84,6 @@ function Filter() {
                 onClick={clearAllFilters}>
                 Clear
               </button>
-              {/* <button
-                type="submit"
-                className={`${styles.filter__navigationBtn + ' ' + styles.btnEffect}`}>
-                Submit
-              </button> */}
             </div>
           </div>
         </div>
