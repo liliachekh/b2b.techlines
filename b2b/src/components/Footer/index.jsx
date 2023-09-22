9e1783717598b931fb9d32f3d80eeecadcca6c98
Liliia Chekh <liliacheh@gmail.com>
2023-09-22 07:29:24 +0200

    change footer styles

--- b2b/src/components/Footer/index.jsx
+++ b2b/src/components/Footer/index.jsx
@@ -10,13 +10,12 @@ export default function Footer() {
             <p className={style.footer__devinfo}>Developed by <Link to="https://othersite.net/" target="blank" className={style.footer__info_link}>OTHERSITE STUDIO</Link></p>
         </div>
         <div className={style.footer__links}>
-            <Link to='/about-us' className={style.footer__links_link}>About us</Link>
-            <Link to='/contacts' className={style.footer__links_link}>Contacts</Link>
-            <Link to='/delivery' className={style.footer__links_link}>Delivery</Link>
-            <Link to='/privacy-policy' className={style.footer__links_link}>Privacy policy</Link>
-            <Link to='/terms-and-conditions' className={style.footer__links_link}>Terms and Conditions</Link>
-            <Link to='/cookie-policy' className={style.footer__links_link}>Cookie policy</Link>
-
+            <Link to='/about-us' className={`${style.footer__links_link} ${style.footer__links_aboutUs}`}>About us</Link>
+            <Link to='/contacts' className={`${style.footer__links_link} ${style.footer__links_contactUs}`}>Contacts</Link>
+            <Link to='/terms-and-conditions' className={`${style.footer__links_link} ${style.footer__links_terms}`}>Terms and Conditions</Link>
+            <Link to='/privacy-policy' className={`${style.footer__links_link} ${style.footer__links_privacy}`}>Privacy policy</Link>
+            <Link to='/delivery' className={`${style.footer__links_link} ${style.footer__links_delivery}`}>Delivery</Link>
+            <Link to='/cookie-policy' className={`${style.footer__links_link} ${style.footer__links_cookie}`}>Cookie policy</Link>
 
         </div>
       </div>
