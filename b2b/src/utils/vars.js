12500dd986e3a1ca74c820a9833721e45a704d50
Liliia Chekh <liliacheh@gmail.com>
2023-09-25 18:31:47 +0200

    change baseUrl

--- b2b/src/utils/vars.js
+++ b2b/src/utils/vars.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://storage.techlines.es/api/';
-// export const baseUrl = 'http://localhost:4000/api/';
+// export const baseUrl = 'https://storage.techlines.es/api/';
+export const baseUrl = 'http://localhost:4000/api/';
