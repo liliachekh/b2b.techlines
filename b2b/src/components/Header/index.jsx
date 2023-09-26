af924add96e464fb1648a6e18032e135d42b61bb
marcik357 <marcik357@gmail.com>
2023-09-26 13:50:53 +0300

    UPDATE: clear header

--- b2b/src/components/Header/index.jsx
+++ b2b/src/components/Header/index.jsx
@@ -6,14 +6,13 @@ import { scrollToTop } from "../../utils";
 import menuData from "../MenuLink/menuData";
 import MenuLink from "../MenuLink";
 import MobilNav from "../MobiNav";
-import { useLogOutMutation } from '../../store/api/customers.api';
-
 
 export default function Header() {
   const [scrolled, setScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [isLogin, setIsLogin] = useState(true);
-  const [logOut] = useLogOutMutation();
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+  const location = useLocation();
 
   useEffect(() => {
     function handleScroll() {
@@ -31,8 +30,6 @@ export default function Header() {
     setIsOpen(!isOpen);
   }
 
-  const isDesktop = useMediaQuery({ minWidth: 768 });
-
   useEffect(() => {
     if (!isDesktop && isOpen) {
       document.body.style.overflow = 'hidden';
@@ -41,8 +38,6 @@ export default function Header() {
     }
   }, [isDesktop, isOpen]);
 
-  const location = useLocation();
-
   const isActive = (path) => {
     if (isLogin) {
       return location.pathname === path;
@@ -50,56 +45,39 @@ export default function Header() {
     return false;
   };
 
-  async function handleLogOut() {
-    await logOut();
-    window.location.reload()
-  }
-
   return (
-    <>
-      <header className={style.header}>
-        <div className={`${style.header__wrapper} ${scrolled && style.header__scrolled}`}>
-          <div className={`${style.header__container} ${scrolled && style.header__container_scrolled}`}>
-            <div className={style.header__section}>
-              <Link to="/" onClick={scrollToTop}>
-                <div className={`${style.logo} ${scrolled && style.logo__scrolled}`}>
-                  <img src="/images/Tech.png" alt="techlines logo" />
-                </div>
-              </Link>
-            </div>
-            <nav className={`${style.nav} ${isOpen && style.active}`}>
-              <ul className={style.nav__list}>
-                {menuData.map(({type, page, text, icon}) => (
-                  (isLogin && type !== 'login') || (!isLogin && type !== 'profile') ? (
-                    <MenuLink
-                      key={type}
-                      classItem={`${style.nav__item}`}
-                      page={page}
-                      isActive={isActive(page)}
-                      classActive={style.activeLink}
-                      closeBurgerMenu={() => toggleBurgerMenu()}
-                      text={text}
-                      isDesktop={isDesktop}
-                      isLogin={isLogin}
-                      icon={icon}
-                      onClick={handleLogOut}
-                      type={type}
-                  />
-                  ) : null
-                  ))}
-              </ul>
-            </nav>
-            <>
-              <MobilNav
-                isLogin={isLogin}
-                isActive={isActive}
-                isOpen={isOpen}
-                toggleBurgerMenu={() => toggleBurgerMenu()}
-              />
-            </>
+    <header className={style.header}>
+      <div className={`${style.header__wrapper} ${scrolled && style.header__scrolled}`}>
+        <div className={`${style.header__container} ${scrolled && style.header__container_scrolled}`}>
+          <div className={style.header__section}>
+            <Link to="/" onClick={scrollToTop}>
+              <div className={`${style.logo} ${scrolled && style.logo__scrolled}`}>
+                <img src="/images/Tech.png" alt="techlines logo" />
+              </div>
+            </Link>
           </div>
+          <nav className={`${style.nav} ${isOpen && style.active}`}>
+            <ul className={style.nav__list}>
+              {menuData.map(({ type, page, text, icon }) => (
+                <MenuLink
+                  key={type}
+                  classItem={`${style.nav__item}`}
+                  page={page}
+                  isActive={isActive(page)}
+                  classActive={style.activeLink}
+                  closeBurgerMenu={() => toggleBurgerMenu()}
+                  text={text}
+                  isDesktop={isDesktop}
+                  icon={icon} />))}
+            </ul>
+          </nav>
+          <MobilNav
+            isLogin={isLogin}
+            isActive={isActive}
+            isOpen={isOpen}
+            toggleBurgerMenu={() => toggleBurgerMenu()} />
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
--- b2b/src/components/MenuLink/index.jsx
+++ b2b/src/components/MenuLink/index.jsx
@@ -2,56 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { ArrowRight } from '../icons';
+import { scrollToTop } from '../../utils';
 
-function MenuLink(props) {
-	const { classItem, page, isActive, classActive, closeBurgerMenu, text, isDesktop, icon, onClick, type } = props;
-
-	const handleIconClick = () => {
-		if (type === 'logout' && onClick){
-      onClick(); 
-    }
+function MenuLink({ classItem, page, isActive, classActive, closeBurgerMenu, text, isDesktop, icon }) {
+  const handleIconClick = () => {
+    scrollToTop();
     if (!isDesktop) {
       closeBurgerMenu();
     }
   };
 
-	return (
-		<li className={`${classItem}`}>
-			<NavLink to={page} className={isActive ? classActive : ''} onClick={handleIconClick}
-			>
-				{icon && isDesktop ?
-					(isActive ? (icon(35, 35, '#202025', '2.2')) : (icon(35, 35, '#202025', '1.5')))
-					:
-					<>
-						<span>{text}</span>
-						{!isDesktop ? <ArrowRight /> : null}
-					</>
-				}
-			</NavLink>
-		</li>
-	);
+  return (
+    <li className={classItem}>
+      <NavLink to={page} className={isActive ? classActive : ''} onClick={handleIconClick}>
+        {icon && isDesktop
+          ? (isActive ? (icon(35, 35, '#202025', '2.2')) : (icon(35, 35, '#202025', '1.5')))
+          : <>
+            <span>{text}</span>
+            {!isDesktop ? <ArrowRight /> : null}
+          </>
+        }
+      </NavLink>
+    </li>
+  );
 }
 
 MenuLink.propTypes = {
-	classItem: PropTypes.string,
-	page: PropTypes.string,
-	isActive: PropTypes.bool,
-	classActive: PropTypes.string,
-	closeBurgerMenu: PropTypes.func,
-	text: PropTypes.string,
-	isDesktop: PropTypes.bool,
-	icon: PropTypes.func,
+  classItem: PropTypes.string,
+  page: PropTypes.string,
+  isActive: PropTypes.bool,
+  classActive: PropTypes.string,
+  closeBurgerMenu: PropTypes.func,
+  text: PropTypes.string,
+  isDesktop: PropTypes.bool,
+  icon: PropTypes.func,
 };
 
 MenuLink.defaultProps = {
-	classItem: '',
-	page: '',
-	isActive: false,
-	classActive: '',
-	closeBurgerMenu: null,
-	text: '',
-	isDesktop: false,
-	icon: null
+  classItem: '',
+  isActive: false,
+  classActive: '',
+  text: '',
+  isDesktop: false,
+  icon: null
 };
 
 export default MenuLink;
