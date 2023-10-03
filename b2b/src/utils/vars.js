7f9cca44f0257dceb95b2ce1344fac8afc7a996e
Nataliia Vorobei <natalia.vorobei27@gmail.com>
2023-10-03 21:09:55 +0300

    UPDATE: minor changes

--- b2b/src/utils/vars.js
+++ b2b/src/utils/vars.js
@@ -1,7 +1,5 @@
-// export const baseUrl = 'https://storage.techlines.es/api/';
-export const baseUrl = 'http://localhost:4000/api/';
-// export const baseUrl = 'https://storage.techlines.es/api/';
-export const baseUrl = 'http://localhost:4000/api/';
+export const baseUrl = 'https://storage.techlines.es/api/';
+// export const baseUrl = 'http://localhost:4000/api/';
 
 export const initialValuesShippingForm = {
   countryName: '',
