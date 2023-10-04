5c07f45475dcc8f89ab51876999112c44de43a46
marcik357 <marcik357@gmail.com>
2023-10-04 20:01:38 +0300

    UPDATE: change empty title

--- b2b/src/hooks/useTitle.js
+++ b2b/src/hooks/useTitle.js
@@ -2,6 +2,6 @@ import { useEffect } from 'react';
 
 export function useTitle(title) {
   useEffect(() => {
-    document.title = title ? `Teclines - ${title}` : 'B2B Teclines';
+    document.title = title ? `B2B Teclines - ${title}` : 'B2B Teclines';
   }, [title]);
 };
