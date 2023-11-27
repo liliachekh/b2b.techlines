b1d33a20a80ce2533097dad5872bac08e42715d6
marcik357 <marcik357@gmail.com>
2023-11-27 21:13:34 +0200

    UPDATE: req without 3dsUrl

--- b2b/src/components/PaymentForm/index.jsx
+++ b2b/src/components/PaymentForm/index.jsx
@@ -12,8 +12,8 @@ export function PaymentForm({ setOrder, orderNo, totalPrice }) {
   // const navigate = useNavigate();
   const [modal, setModal] = useState(null);
   const [payment, setPayment] = useState(null);
-const [threeDSMethodData, setThreeDSMethodData] = useState(null);
-const [threeDSMethodURL, setThreeDSMethodURL] = useState(null);
+  const [threeDSMethodData, setThreeDSMethodData] = useState(null);
+  const [threeDSMethodURL, setThreeDSMethodURL] = useState(null);
 
   const [deleteCart] = useDeleteCartMutation();
 
@@ -50,11 +50,43 @@ const [threeDSMethodURL, setThreeDSMethodURL] = useState(null);
           body: JSON.stringify(reqObj)
         })
       if (!res.ok) console.log('Error in payment')
-      if (res.ok) { 
+      if (res.ok) {
         const responseData = await res.json();
-        setThreeDSMethodData (responseData.threeDSMethodData)
-        if(responseData.threeDSMethodURL) setThreeDSMethodURL(responseData.threeDSMethodURL)
-        setPayment('ok')}
+        console.log(responseData);
+
+        setThreeDSMethodData(responseData.threeDSMethodData)
+
+        if (responseData.threeDSMethodURL) {
+          // setThreeDSMethodURL(responseData.threeDSMethodURL)
+        } else {
+          const browser = {
+            "threeDSInfo": "AuthenticationData",
+            "protocolVersion": responseData.protocolVersion,
+            "browserAcceptHeader": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,application/json",
+            "browserUserAgent": window.navigator.userAgent,
+            "browserJavaEnabled": window.navigator.javaEnabled() || false,
+            "browserLanguage": window.navigator.language,
+            "browserColorDepth": window.screen.pixelDepth,
+            "browserScreenHeight": window.screen.availHeight,
+            "browserScreenWidth": window.screen.availWidth,
+            "browserTZ": new Date().getTimezoneOffset(),
+            "threeDSServerTransID": responseData.threeDSServerTransID,
+            "notificationURL": "https://dev.techlines.es/api/payment/3DS",
+            "threeDSCompInd": "Y"
+          }
+
+          console.log({ ...reqObj, "DS_MERCHANT_EMV3DS": { ...browser } });
+          const res = await fetch('http://localhost:4000/api/payment/authorization',
+            {
+              method: 'POST',
+              headers: { "Content-Type": "application/json" },
+              credentials: 'include',
+              body: JSON.stringify({ ...reqObj, "DS_MERCHANT_EMV3DS": { ...browser } })
+            })
+          console.log(res);
+        }
+        setPayment('ok')
+      }
       // window.removeEventListener("message", receiveMessage);
     }
   }
@@ -69,7 +101,7 @@ const [threeDSMethodURL, setThreeDSMethodURL] = useState(null);
       {modal === 'ok' &&
         <AnimatePresence>
           <motion.div className={styles.modal} onClick={closeModal} role='button' {...animateModal}>
-          {/* <motion.div className={styles.modal} role='button' {...animateModal}> */}
+            {/* <motion.div className={styles.modal} role='button' {...animateModal}> */}
             <div className={styles.modal__wrapper}>
               <div className={styles.modal__text}>
                 Your order has been processed and invoice has been sent to you by email
@@ -91,7 +123,7 @@ const [threeDSMethodURL, setThreeDSMethodURL] = useState(null);
         <h3 className={styles.form__title}>Payment form</h3>
         <div id="card-form" style={{ height: '400px' }} />
 
-        {threeDSMethodURL && <Payment3DS threeDSMethodData={threeDSMethodData} threeDSMethodURL={threeDSMethodURL}/>}
+        {threeDSMethodURL && <Payment3DS threeDSMethodData={threeDSMethodData} threeDSMethodURL={threeDSMethodURL} />}
 
         <form name="datos">
           <input type="hidden" id="token"></input>
