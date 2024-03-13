289326be2beecdca21fbb0eba77773095c62dc7e
Liliia Chekh <liliacheh@gmail.com>
2024-03-13 15:39:47 +0100

    chamge tier prices

--- b2b/src/hooks/useTierPrice.js
+++ b2b/src/hooks/useTierPrice.js
@@ -6,11 +6,11 @@ export function useTierPrice() {
   return (currentPrice) => {
     switch (customer.tier) {
       case 'vip':
-        return (Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.005) * 100) / 100).toFixed(2)
-      case 'premium':
         return (Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.01) * 100) / 100).toFixed(2)
-      case 'beginner':
+      case 'premium':
         return (Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.02) * 100) / 100).toFixed(2)
+      case 'beginner':
+        return (Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.03) * 100) / 100).toFixed(2)
 
       default:
         break;
