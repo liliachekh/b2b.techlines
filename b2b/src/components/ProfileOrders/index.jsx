6801a5cdb213ca15eedd7c2f4e7630538ae11db0
Liliia Chekh <liliacheh@gmail.com>
2024-05-09 13:24:33 +0200

    add order date

--- b2b/src/components/ProfileOrders/index.jsx
+++ b2b/src/components/ProfileOrders/index.jsx
@@ -3,7 +3,7 @@ import { useGetOrdersQuery } from '../../store/api/order.api';
 import ProductCard from '../ProductCard';
 import Loader from '../Loader';
 import { useTitle } from '../../hooks';
-import { Link } from 'react-router-dom';
+import { formatDate } from '../../utils';
 
 export function ProfileOrders() {
   useTitle('Profile | Orders');
@@ -13,15 +13,13 @@ export function ProfileOrders() {
 
   return (
     <>
-      {orders && orders?.map(({ products, orderNo, totalSum, status, paymentInfo, discount, deliveryPrice }) => (
-        <Link to={`/orders/${orderNo}`}
-        title="View order"
-        target="_blank">
+      {orders && orders?.map(({ products, orderNo, totalSum, status, paymentInfo, discount, deliveryPrice, date }) => (
           <div className={styles.order} key={orderNo}>
           {products?.map(({ product, cartQuantity }) => (
             <ProductCard {...product} displayTable={true} key={product?._id} orderQuantity={cartQuantity} />))}
           <div className={styles.order__info}>
             <div className={styles.order__text}>Order №: <span className={styles.order__text_value}>{orderNo}</span></div>
+            <div className={styles.order__text}>Order date: <span className={styles.order__text_value}>{formatDate(date)}</span></div>
             <div className={styles.order__text}>Status: <span className={styles.order__text_value}>{status}</span></div>
             <div className={styles.order__text}>Payment method: <span className={styles.order__text_value}>{paymentInfo === "CARD" ? 'Card (+1.7%)' : 'IBAN'}</span></div>
             {discount > 0 && <div className={styles.order__text}>Discount: <span className={styles.order__text_value}>-{discount} €</span></div>}
@@ -32,7 +30,7 @@ export function ProfileOrders() {
             <div className={`${styles.order__text} ${styles.order__text_total}`}>Total Sum: <span className={styles.order__text_value}>{totalSum} €</span></div>
           </div>
         </div>
-        </Link>))}
+      ))}
     </>
   )
 }
